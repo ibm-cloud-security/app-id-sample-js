95361529b9a80d1f855eda8be1c56eafa7b232bb
Bernie <bernhardtramat@gmail.com>
2019-11-05 08:14:23 -0700

    Added silent login and change password

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import AppID from 'ibmcloud-appid-js';
 import config from './config.json';
 
 const $loginButton = document.getElementById('login');
+const $changePasswordButton = document.getElementById('password');
 const $welcome = document.getElementById('welcome');
 const $afterLogin = document.getElementById('afterLogin');
 const $welcomeNameId = document.getElementById('welcomeNameID');
 const $tokenContent = document.getElementById('tokenContent');
 const $userContent = document.getElementById('userContent');
 const $error = document.getElementById('error');
-
 const appID = new AppID();
 
 async function onLoginButtonClick() {
@@ -16,18 +16,23 @@ async function onLoginButtonClick() {
 		hideElement($loginButton);
 
 		const tokens = await appID.signin();
+		
 		let userInfo = await appID.getUserInfo(tokens.accessToken);
-
+		let jwtToken = tokens.idToken
 		hideElement($welcome);
+		changePassword(jwtToken);
 		showElement($afterLogin);
 
 		let decodeIDToken = tokens.idTokenPayload;
-
 		$welcomeNameId.textContent = 'Hi ' + decodeIDToken.name + ', Congratulations!';
 		$tokenContent.textContent = JSON.stringify(decodeIDToken);
 		$userContent.textContent = JSON.stringify(userInfo);
 	} catch (e) {
-		$error.textContent = e;
+		if (e == 'Error: Popup closed') {
+			$error.textContent = 'The pop-up window closed before sign-in completed. Click Sign in to try again.';
+		} else {
+			$error.textContent = e;
+		}
 		showElement($loginButton);
 	}
 }
@@ -35,15 +40,31 @@ async function onLoginButtonClick() {
 (async () => {
 	try {
 		await appID.init(config);
+		const tokens = await appID.silentSignin();
+		let userInfo = await appID.getUserInfo(tokens.accessToken);
+		let decodeIDToken = tokens.idTokenPayload;
+		$welcomeNameId.textContent = 'Hi ' + decodeIDToken.name + ', Congratulations!';
+		$tokenContent.textContent = JSON.stringify(decodeIDToken);
+		$userContent.textContent = JSON.stringify(userInfo);
+		
+	} catch (e) {
 		showElement($loginButton);
 		$loginButton.addEventListener('click', onLoginButtonClick);
-	} catch (e) {
-		$error.textContent = e;
 	}
 })();
 
 function hideElement($element) {
 	$element.classList.add('hidden');
+}
+
+function changePassword(idToken) {
+	$changePasswordButton.addEventListener('click', async function(){
+		try {
+			let tokens = await appID.changePassword(idToken);
+		} catch (e) {
+			console.log(e)
+		}
+	});
 }
 
 function showElement($element) {
