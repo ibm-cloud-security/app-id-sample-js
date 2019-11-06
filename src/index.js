f214e116e3b3855db0122add9da4a120c5449f02
Bernie <bernhardtramat@gmail.com>
2019-11-06 15:01:13 -0700

    Added displays on successful silent logins and password changes

--- src/index.js
+++ src/index.js
@@ -9,25 +9,16 @@ const $welcomeNameId = document.getElementById('welcomeNameID');
 const $tokenContent = document.getElementById('tokenContent');
 const $userContent = document.getElementById('userContent');
 const $error = document.getElementById('error');
+const $passwordChange = document.getElementById('passwordChange');
+hideElement($passwordChange);
 
 const appID = new AppID();
 
 async function onLoginButtonClick() {
 	try {
 		hideElement($loginButton);
-
 		const tokens = await appID.signin();
-		let userInfo = await appID.getUserInfo(tokens.accessToken);
-		let jwtToken = tokens.idToken;
-		changePassword(jwtToken);
-		hideElement($welcome);
-		showElement($afterLogin);
-
-		let decodeIDToken = tokens.idTokenPayload;
-
-		$welcomeNameId.textContent = 'Hi ' + decodeIDToken.name + ', Congratulations!';
-		$tokenContent.textContent = JSON.stringify(decodeIDToken);
-		$userContent.textContent = JSON.stringify(userInfo);
+		displayUserInfo(tokens);
 	} catch (e) {
 		if (e == 'Error: Popup closed') {
 			$error.textContent = 'The pop-up window closed before sign-in completed. Click Sign in to try again.';
@@ -42,21 +33,31 @@ async function onLoginButtonClick() {
 	try {
 		await appID.init(config);
 		const tokens = await appID.silentSignin();
-		let userInfo = await appID.getUserInfo(tokens.accessToken);
-		let decodeIDToken = tokens.idTokenPayload;
-		$welcomeNameId.textContent = 'Hi ' + decodeIDToken.name + ', Congratulations!';
-		$tokenContent.textContent = JSON.stringify(decodeIDToken);
-		$userContent.textContent = JSON.stringify(userInfo);
+		displayUserInfo(tokens);
 	} catch (e) {
 		showElement($loginButton);
 		$loginButton.addEventListener('click', onLoginButtonClick);
 	}
 })();
 
+async function displayUserInfo(tokens) {
+	let userInfo = await appID.getUserInfo(tokens.accessToken);
+	let decodeIDToken = tokens.idTokenPayload;
+	let jwtToken = tokens.idToken;
+	changePassword(jwtToken);
+	hideElement($welcome);
+	showElement($afterLogin);
+	$welcomeNameId.textContent = 'Hi ' + decodeIDToken.name + ', Congratulations!';
+	$tokenContent.textContent = JSON.stringify(decodeIDToken);
+	$userContent.textContent = JSON.stringify(userInfo);
+}
+
 function changePassword(idToken) {
 	$changePasswordButton.addEventListener('click', async function(){
+		console.log('here')
 		try {
 			let tokens = await appID.changePassword(idToken);
+			showElement($passwordChange);
 		} catch (e) {
 			console.log(e)
 		}
